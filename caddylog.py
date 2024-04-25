13e8b678c7057d9a516ad216cd04df0640a9e5b8
Homer Simpson <hb@hb.gl>
2024-04-25 23:57:46 +0200

    loop only where ip is not null

--- caddylog.py
+++ caddylog.py
@@ -7,12 +7,15 @@ def main():
             try:
                 json_obj = json.loads(line)
                 # extract 'request' element from json, then extract 'remote_ip' element from the request:
-                print(json_obj['request']['remote_ip'])
+                print(json_obj["request"]["remote_ip"])
+                # if remote_ip  is null continue loop
+                if json_obj["request"]["remote_ip"] is None:
+                    continue
                 json_str = json.dumps(json_obj)
                 print(f"Length of JSON string: {len(json_str)}")
-            except (json.JSONDecodeError) as e:
+            except json.JSONDecodeError as e:
                 print(f"Error: Not JSON")
-            except (KeyError) as e:
+            except KeyError as e:
                 print(f"json object error (key not found) {e}")
 
 
